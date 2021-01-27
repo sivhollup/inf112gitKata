48e84fbf63cfeaca06d8b52f5592ba71923bde6a
Siv Midtun Hollup <siv.hollup@gmail.com>
2021-01-27 09:31:18 +0100

    Oppgradert til Junit5

--- src/test/java/inf112/git/FizzBuzzTest.java
+++ src/test/java/inf112/git/FizzBuzzTest.java
@@ -1,34 +1,6 @@
 package inf112.git;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class FizzBuzzTest {
 
-    private FizzBuzz fizzBuzz;
-
-    @Before
-    public void setUp() {
-        fizzBuzz = new FizzBuzz();
-    }
-
-    @Test
-    public void lowerBoundRegularNumber() {
-        assertEquals("1", fizzBuzz.compute(1));
-    }
-
-    @Test
-    public void convert2ToStringRepresentation() {
-        assertEquals("2", fizzBuzz.compute(2));
-    }
-
-    @Test
-    public void convert3ToFizz() {
-        assertEquals("Fizz", fizzBuzz.compute(3));
-    }
 }
